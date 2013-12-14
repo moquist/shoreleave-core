e563abc7964d9d4baf0ec83aff3272beaa712a15
Matt Oquist <moquist@majen.net>
2013-12-14 13:46:32 -0500

    removed lein-marginalia and clj 1.4.0 deps

--- project.clj
+++ project.clj
@@ -1,13 +1,8 @@
-(defproject shoreleave/shoreleave-core "0.3.0"
+(defproject shoreleave/shoreleave-core "0.3.1-SNAPSHOT"
   :description "A smarter client-side with ClojureScript : Shoreleave's core auxiliary functions"
   :url "http://github.com/shoreleave"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "See the notice in README.mkd or details in LICENSE_epl.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [;[cdt "1.2.6.2-SNAPSHOT"]
-                     ;[lein-cdt "1.0.0"] ; use lein cdt to attach
-                     ;[lein-autodoc "0.9.0"]
-                     [lein-marginalia "0.7.1"]])
+            :comments "See the notice in README.mkd or details in LICENSE_epl.html"})
 
